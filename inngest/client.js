170513a473e0781fa3d941d2aae1a87f6536581c
samuel <samueleloh91@gmail.com>
2025-12-14 02:58:02 +0000

    yes dit that

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,30 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "urban-express" });
+export const inngest = new Inngest({ id: "gocart" });
+
+import { inngest } from "./client";
+
+export const syncUserCreations = inngest.createFunction(
+  { id: "sync-user-creations" },
+  { event: "user/created" },
+  async ({ event }) => {
+    return { success: true };
+  }
+);
+
+export const syncUserUpdation = inngest.createFunction(
+  { id: "sync-user-updated" },
+  { event: "user/updated" },
+  async ({ event }) => {
+    return { success: true };
+  }
+);
+
+export const syncUserDeletion = inngest.createFunction(
+  { id: "sync-user-deleted" },
+  { event: "user/deleted" },
+  async ({ event }) => {
+    return { success: true };
+  }
+);
